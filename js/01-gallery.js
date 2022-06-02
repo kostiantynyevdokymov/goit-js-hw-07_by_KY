d4ecd918014b15eda160872d1f8c252ae2d6403c
Kostiantyn <cashalot80@ukr.net>
2022-06-02 10:25:13 +0100

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,12 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
+const gallery = document.querySelector(`.gallery`);
+galleryItems.addEventListener(`click`, selectImg)
 console.log(galleryItems);
+
+function createGallery() {
+    const items = [];
+    for (let i = 0; i <= 9; i += 1) {
+        
+    }
+}
