2832471166f52da873fabcdaf586625582c9672f
Kostiantyn <cashalot80@ukr.net>
2022-06-02 12:12:36 +0100

    work task#1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,12 +1,38 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-const gallery = document.querySelector(`.gallery`);
-galleryItems.addEventListener(`click`, selectImg)
-console.log(galleryItems);
+const galleryCont = document.querySelector("div.gallery");
+const galleryMarkUp = createGalleryImg(galleryItems);
 
-function createGallery() {
-    const items = [];
-    for (let i = 0; i <= 9; i += 1) {
-        
+galleryCont.insertAdjacentHTML("beforeend", galleryMarkUp);
+
+function createGalleryImg(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+        return `
+        <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+        />
+    </a>
+    </div>
+    `;
+        })
+        .join("");
     }
-}
+    galleryCont.addEventListener(`click`, openImg)
+    
+    function openImg(event) {
+        event.preventDefault();
+        if (!event.target.classList.contains("gallery__item")) {
+            return;
+        }
+        const instanse = basicLightBox.create(
+            `<img src="${event.target.dataset.source}" alt="${event.target.dataset.description}">`
+        );
+            instanse.show();
+    }
+
